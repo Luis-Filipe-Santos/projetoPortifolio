eca824a288a0eacca6216619927ef1102238860c
Luis Filipe Santos Silva <109242677+Luis-Filipe-Santos@users.noreply.github.com>
2023-02-06 16:41:15 -0300

    Fazendo a tela de preloader

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,10 +1,9 @@
-var btnContact = document.querySelector('.jl-btn-contact');
+var btnContact = document.querySelector(".jl-btn-contact");
 
-btnContact.addEventListener('click', function() {
-    var boxContact = document.querySelector('.jl-contact-info');
+btnContact.addEventListener("click", function () {
+  var boxContact = document.querySelector(".jl-contact-info");
 
-    boxContact.classList.toggle('jl-is-open');
-    
-    this.classList.toggle('jl-change-icon');
+  boxContact.classList.toggle("jl-is-open");
 
+  this.classList.toggle("jl-change-icon");
 });

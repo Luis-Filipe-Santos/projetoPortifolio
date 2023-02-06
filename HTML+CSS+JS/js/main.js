438f6cbf1efb7a99b78b3bb5c299af73385311bc
Luis Filipe Santos Silva <109242677+Luis-Filipe-Santos@users.noreply.github.com>
2023-02-06 19:16:44 -0300

    Fazendo o Javascript da tela de preloader

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,5 +1,15 @@
+// Declarando variáveis
 var btnContact = document.querySelector(".jl-btn-contact");
 
+// Page PreLoader
+window.addEventListener('load', function() {
+    var pagePreloader = document.querySelector('.jl-preloader');
+    
+    pagePreloader.classList.add("jl-fade-out");
+    
+}); 
+
+// Abrindo e Fechando informações de contato 
 btnContact.addEventListener("click", function () {
   var boxContact = document.querySelector(".jl-contact-info");
 
